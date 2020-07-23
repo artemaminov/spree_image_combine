65be1c1a75631a77f77bd42415dddc389106c133
artemaminov <artemaminov@gmail.com>
2020-07-23 19:16:36 +0500

    fix rename model_name to anchor_model

--- db/migrate/20200723073246_create_spree_image_combine_block_positions.rb
+++ db/migrate/20200723073246_create_spree_image_combine_block_positions.rb
@@ -2,7 +2,7 @@ class CreateSpreeImageCombineBlockPositions < ActiveRecord::Migration[5.2]
   def change
     create_table :spree_image_combine_block_positions do |t|
       t.string :anchor_uid
-      t.string :model_name
+      t.string :anchor_model
 
       t.timestamps
     end
