7871cf21f9593e6191616c3b3b22de1678fcb989
artemaminov <artemaminov@gmail.com>
2020-07-24 15:03:19 +0500

    add model_class_name column

--- db/migrate/20200723073722_create_spree_image_combine_block_types.rb
+++ db/migrate/20200723073722_create_spree_image_combine_block_types.rb
@@ -2,6 +2,7 @@ class CreateSpreeImageCombineBlockTypes < ActiveRecord::Migration[5.2]
   def change
     create_table :spree_image_combine_block_types do |t|
       t.string :name, null: false
+      t.string :model_class_name, null: false
       t.integer :width, null: false
       t.integer :height, null: false
 
