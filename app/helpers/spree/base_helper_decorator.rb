17995150571598f6957f98631a744d4705109214
artemaminov <artemaminov@gmail.com>
2020-08-25 10:34:28 +0500

    fallback if no product images found

--- app/helpers/spree/base_helper_decorator.rb
+++ app/helpers/spree/base_helper_decorator.rb
@@ -2,14 +2,20 @@ Spree::BaseHelper.module_eval do
   def image_combine_tag(object, image_class = '')
     image = Spree::ImageCombine.fetch(object, controller_name)
     content_tag :picture do
+      output =[]
       if image.blank?
-        return nil
+        if object.is_a?(Spree::Product)
+          main_image = product_image(object)
+        else
+          return nil
+        end
       else
         output = Spree::CropperDimension.dimensions.map { |device, dimensions|
           content_tag :source, "", { media: "(max-width: #{ dimensions[:width] }px)", srcset: main_app.url_for(image.attachment.variant(crop: image.for(device)))}
         }
-        output << [image_tag(main_app.url_for(image.attachment.variant(crop: image.for(Spree::CropperDimension.largest))), class: image_class)]
+        main_image = image_tag(main_app.url_for(image.attachment.variant(crop: image.for(Spree::CropperDimension.largest))), class: image_class)
       end
+      output << main_image
       safe_join(output, "\n")
     end
   end
