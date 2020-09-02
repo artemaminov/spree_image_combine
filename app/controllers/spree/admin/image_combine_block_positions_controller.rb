0c0606231785fc1ac7e6c6bc4c81886d34ccf496
artemaminov <artemaminov@gmail.com>
2020-09-02 15:14:47 +0500

    fix create new BlockPosition

--- app/controllers/spree/admin/image_combine_block_positions_controller.rb
+++ app/controllers/spree/admin/image_combine_block_positions_controller.rb
@@ -1,6 +1,12 @@
 module Spree
   module Admin
     class ImageCombineBlockPositionsController < ResourceController
+      before_action :load_data
+
+      def new
+        @records = []
+      end
+
       def edit
         # Get all records of ImageCombine's BlockType selected model
         @records = Object.const_get(@image_combine_block_position.block_type.model_class_name).all
