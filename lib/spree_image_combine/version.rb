44c4b08d2cf3fb4f97b6fca947d229b782ec80b7
artemaminov <artemaminov@gmail.com>
2020-09-16 14:41:32 +0500

    bump 0.2.0.alpha

--- lib/spree_image_combine/version.rb
+++ lib/spree_image_combine/version.rb
@@ -9,8 +9,8 @@ module SpreeImageCombine
 
   module VERSION
     MAJOR = 0
-    MINOR = 1
-    TINY  = 1
+    MINOR = 2
+    TINY  = 0
     PRE   = 'alpha'.freeze
 
     STRING = [MAJOR, MINOR, TINY, PRE].compact.join('.')
